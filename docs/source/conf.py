d9c9dcf951afefc04261f05fb490b67fa14831d0
jotalexvalencia <jorgealexandervalencia@gmail.com>
2022-06-16 08:49:58 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'La Ofrenda'
+copyright = '2022, La Ofrenda'
+author = 'Jorge Alexander Valencia Valencia'
 
 release = '0.1'
 version = '0.1.0'
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'bizstyle'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
